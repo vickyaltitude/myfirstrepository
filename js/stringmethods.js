899c6e6978b49916f2846c89710ee2229fb28748
vickyaltitude <vignvick3005@gmail.com>
2025-11-28 20:48:31 +0530

    Learnt all string methods along with how to reverse a string in javascript in oneline

--- js/stringmethods.js
+++ js/stringmethods.js
@@ -38,3 +38,39 @@ let indO = "this is indexof method";
 
 console.log(indO.indexOf("h"));
 console.log(indO.lastIndexOf("h"));
+
+//slice()
+let str = "this is slice method";
+console.log(str.slice(4, 12));
+console.log(str.slice(2));
+
+//substring()
+//the difference between substring and slice is substring don't support negative value
+let myStr1 = "this is substring";
+console.log(myStr1.substring(0, 5));
+console.log(myStr1.substring(9));
+
+//replace(),replaceAll()
+let repl = "hello world";
+console.log(repl.replace("world", "vignesh"));
+console.log(repl.replaceAll("hello world", "hi everyone"));
+
+//split()
+let spl = "this is split";
+console.log(spl.split("s"));
+
+//concat()
+let con = "hello";
+console.log(con.concat(" world", " hi", " hello"));
+console.log(con + " vignesh");
+
+//startswith(),endsWith()
+let starts = "filename.js";
+console.log(starts.startsWith("fe"));
+console.log(starts.endsWith(".js"));
+
+//repeat()
+console.log("ha".repeat(4));
+
+//oneline solution to reverse the string in javascript
+console.log("this is slice method".split("").reverse().join(""));
