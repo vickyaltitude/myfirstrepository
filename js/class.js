9bf8d6ee483d21eb2ce93d121ca672b9da1da7be
vickyaltitude <vignvick3005@gmail.com>
2025-12-03 20:41:12 +0530

    Learnt static and private identifier inside class

--- js/class.js
+++ js/class.js
@@ -1,10 +1,18 @@
 //Class is a blueprint to create an object in javascript
 class Student {
+  static schoolName = "ABC hr secondary school";
+
+  #age;
+
   constructor(name, age, place, address) {
     this.name = name;
-    this.age = age;
+    this.#age = age;
     this.place = place;
     this.address = address;
+  }
+
+  tellAge() {
+    return this.#age;
   }
 
   greet() {
@@ -16,7 +24,8 @@ let student1 = new Student("senthil", 25, "india", "chennai");
 let student2 = new Student("karthik", 26, "india", "pondy");
 let student3 = new Student("deepa", 28, "india", "cbe");
 let student4 = new Student("siva", 22, "india", "banglore");
-console.log(student1);
-console.log(student2);
+//console.log(Student.schoolName);
+console.log(student1.tellAge());
+/* console.log(student2);
 console.log(student3);
-console.log(student4);
+console.log(student4); */
