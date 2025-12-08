5319da84d6222a3a9f4e3e7695cd70aa9009a4d7
vickyaltitude <vignvick3005@gmail.com>
2025-12-08 21:30:23 +0530

    Learnt user input functions

---
+++ js/userInput.js
@@ -0,0 +1,8 @@
+//let consoleVal = alert("Hello world");
+//console.log(consoleVal);
+
+//let usersName = prompt("Please enter your name: ");
+//console.log(usersName);
+
+//let confirmConsole = confirm("are you sure, you want to log out?");
+//console.log(confirmConsole);
