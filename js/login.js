7ce4844f6695a101998894e8c5bdaf3417b5332a
vickyaltitude <vignvick3005@gmail.com>
2025-12-10 20:45:26 +0530

    Learnt real world login example with javascript

--- js/login.js
+++ js/login.js
@@ -1,7 +1,25 @@
 let btnSubmit = document.querySelector("#btn");
 
 btnSubmit.addEventListener("click", function (event) {
-  console.log(event);
   event.preventDefault();
-  console.log("button clicked");
+
+  let inputEmail = document.querySelector("#email");
+  let emailValue = inputEmail.value;
+  let inputPassword = document.querySelector("#password");
+  let passwordValue = inputPassword.value;
+  let alertElement = document.querySelector("#alert");
+
+  if (!emailValue.trim().includes("@")) {
+    alertElement.textContent = "Please enter a valid email address";
+    alertElement.style.color = "red";
+    return;
+  }
+  if (passwordValue.trim().length < 8) {
+    alertElement.textContent = "Please enter minimum 8 characters";
+    alertElement.style.color = "red";
+    return;
+  }
+
+  alertElement.textContent = "Login successful";
+  alertElement.style.color = "green";
 });
