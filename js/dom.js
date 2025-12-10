c976cbe7cdeb18195b3eb59694ff68bb3baf5a4c
vickyaltitude <vignvick3005@gmail.com>
2025-12-10 19:59:25 +0530

    Learnt some eventlisteners and some methods in dom

--- js/dom.js
+++ js/dom.js
@@ -21,10 +21,10 @@ console.log(document.querySelector("#div").innerHTML);
   '<h2 style="color:red">Heading added using innerHTML</h2><h2 style="color:red">Heading added using innerHTML</h2>'; */
 
 //style
-
-document.querySelector("#ptag").style.color = "blue";
-document.querySelector("#ptag").style.backgroundColor = "black";
-document.querySelector("#ptag").style.fontSize = "30px";
+let ptag = document.querySelector("#ptag");
+ptag.style.color = "blue";
+ptag.style.backgroundColor = "black";
+ptag.style.fontSize = "30px";
 console.log(document.querySelector("#div").innerHTML);
 //console.log(document.querySelector(".remove").classList.remove("remove"));
 console.log(document.querySelector("#div").innerHTML);
@@ -33,6 +33,56 @@ console.log(document.querySelector("#div").innerHTML);
 console.log(document.querySelector("#ptag").getAttribute("class"));
 document.querySelector("#ptag").setAttribute("class", "newclass");
 console.log(document.querySelector("#ptag").getAttribute("class"));
-document.querySelector("#btn").addEventListener("click", function () {
-  document.querySelector("#btn-text").textContent = "Button Clicked!";
+
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function () {
+  let btnText = document.querySelector("#btn-text");
+  btnText.textContent = "Button Clicked!";
 });
+
+let mouseover = document.querySelector("#mouseover");
+mouseover.addEventListener("mouseover", function () {
+  mouseover.style.color = "red";
+  mouseover.style.backgroundColor = "black";
+});
+
+mouseover.addEventListener("mouseout", function () {
+  mouseover.style.color = "black";
+  mouseover.style.backgroundColor = "white";
+});
+
+//createElement, appendChild, removeChild
+
+let newElement = document.createElement("ul");
+let firstChild = document.createElement("li");
+let secondChild = document.createElement("li");
+let thirdChild = document.createElement("li");
+let fourthChild = document.createElement("li");
+
+firstChild.textContent = "HTML";
+secondChild.textContent = "CSS";
+thirdChild.textContent = "JS";
+fourthChild.textContent = "ReactJS";
+
+let body = document.querySelector("body");
+body.appendChild(newElement);
+newElement.appendChild(firstChild);
+newElement.appendChild(secondChild);
+newElement.appendChild(thirdChild);
+newElement.appendChild(fourthChild);
+
+/* firstChild.style.color = "red";
+secondChild.style.color = "blue";
+thirdChild.style.color = "green";
+fourthChild.style.color = "purple"; */
+
+//Loop to style li elements
+
+let liElements = document.querySelectorAll("li");
+
+for (let i = 0; i < liElements.length; i++) {
+  liElements[i].style.color = "orange";
+}
+
+//style by selecting nth child;
