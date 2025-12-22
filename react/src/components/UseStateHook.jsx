1e38f421227107f1d8a61e38e9f605012b3c3e07
vickyaltitude <vignvick3005@gmail.com>
2025-12-22 20:30:00 +0530

    Learnt mapping for different data types in state

--- react/src/components/UseStateHook.jsx
+++ react/src/components/UseStateHook.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import { useState } from "react";
 
 function UseStateHook() {
-  let [count, setCount] = useState(0);
-
+  let [details, setDetails] = useState({
+    name: "vignesh",
+    age: 10,
+    place: "india",
+  });
+  console.log(Object.entries(details));
   return (
     <div>
       <h1>Increase Count</h1>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Plus 1</button>
+      <ul>
+        {Object.entries(details).map((items, index) => (
+          <li key={index}>
+            {items[0]}:{items[1]}
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => setDetails({ ...details, hobbies: "cooking" })}>
+        Add cooking
+      </button>
     </div>
   );
 }
