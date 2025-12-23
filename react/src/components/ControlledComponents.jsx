fa02fb3aee6d0792a851ba51b5a11f67955e8df4
vickyaltitude <vignvick3005@gmail.com>
2025-12-23 20:21:17 +0530

    learnt controlled vs uncontrolled components

--- react/src/components/ControlledComponents.jsx
+++ react/src/components/ControlledComponents.jsx
@@ -1,17 +1,43 @@
-import React from "react";
-import { useRef } from "react";
+import React, { useEffect, useState } from "react";
+//import { useRef } from "react";
 
 function ControlledComponents() {
-  let reference = useRef();
-
+  /*  let reference = useRef();
+let anotherReference = useRef();
   function changeValue() {
-    reference.current.innerText = "hello world";
+    console.log(reference.current.value);
+  } */
+
+  let [userName, setUserName] = useState("");
+
+  function onChangeValue(e) {
+    setUserName(e.target.value);
   }
+
+  useEffect(() => {
+    console.log("im useeffect");
+  });
 
   return (
     <div className="div">
-      <button onClick={changeValue}>Click me</button>
-      <p ref={reference}>Hello</p>
+      <h1>Controlled components vs uncontrolled components</h1>
+      <input
+        /* ref={reference} */ value={userName}
+        onChange={onChangeValue}
+        type="text"
+      />
+      <input
+        /* ref={anotherReference} */ value={userName}
+        onChange={onChangeValue}
+        type="text"
+      />
+      <h2>Uncontrolled components</h2>
+      <p>
+        When you access the value directly using the dom elements with the help
+        of useRef hook that is uncontrolled components.
+      </p>
+      <button>Click me</button>
+      <p>Hello</p>
     </div>
   );
 }
