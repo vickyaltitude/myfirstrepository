498948f5f4a433a1192a4cbed53b3bd33f0d84bb
vickyaltitude <vignvick3005@gmail.com>
2026-01-09 20:40:47 +0530

    Learnt endpoints from request and sent response

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,8 +2,14 @@ const http = require("http");
 const { myName, myPlace, greet } = require("./filehandling");
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end(`Hello, ${myName} from ${myPlace}! ${greet()}`);
+  console.log("Request received:", request.url);
+  if (request.url === "/") {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end(`Hello, ${myName} from ${myPlace}! ${greet()}`);
+  } else if (request.url === "/about") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ name: myName, place: myPlace }));
+  }
 });
 
 server.listen(3000, () => {
